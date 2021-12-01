14fa41892ce49231edd71e20318e01e410f1bcb4
galuropek <Z322200119i>
2021-12-01 17:48:12 +0300

    User can view a question list

--- spec/features/question/index_spec.rb
+++ spec/features/question/index_spec.rb
@@ -7,7 +7,7 @@ feature 'User can view a questions list' do
     visit questions_path
 
     expect(page).to have_content 'Questions list:'
-    expect(page).to have_content 'Question 1'
-    expect(page).to have_content 'Question 2'
+    expect(page).to have_content questions.first.title
+    expect(page).to have_content questions.last.title
   end
 end
