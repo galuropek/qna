97dc49055240d470b045255e6ac12ac401601660
galuropek <Z322200119i>
2021-12-02 13:56:42 +0300

    User can view question's page and question's answers

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :answer do
     title { "MyString" }
     body { "MyText" }
+    question
 
     trait :invalid do
       title { nil }
---
+++ spec/features/question/show_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature 'User can view question and answers', %q{
+  User goes to the question page
+  View question's attributes and answers of this question
+} do
+  given(:question) { create(:question) }
+  given!(:answers) { create_list(:answer, 3, question: question) }
+
+  scenario "User views question's attributes" do
+    visit question_path(question)
+
+    expect(page).to have_content question.title
+    expect(page).to have_content question.body
+  end
+
+  scenario "User views answer no the question's page" do
+    visit question_path(question)
+
+    answers.each do |answer|
+      expect(page).to have_content answer.title
+      expect(page).to have_content answer.body
+    end
+  end
+end
