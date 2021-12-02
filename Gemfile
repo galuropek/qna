59eb9b39991e5c387241e1a513802a79b2af0d33
galuropek <Z322200119i>
2021-12-02 14:48:12 +0300

    User can sign in

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 gem 'slim-rails'
+gem 'devise'
 
 # Controller simplification
 gem 'decent_exposure', '~> 3.0'
---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+feature 'User can sign in', %q{
+  In order to ask questions
+  As an unauthenticated user
+  I`d like to be able to sign in
+} do
+
+  given(:user) { User.create!(email: 'user@test.com', password: 'test1234') }
+
+  background { visit new_user_session_path }
+
+  scenario 'Registred user tries to sign in' do
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Log in'
+
+    expect(page).to have_content 'Signed in successfully.'
+  end
+
+  scenario 'Unregistred user tries to sign in' do
+    fill_in 'Email', with: 'wrong@test.com'
+    fill_in 'Password', with: 'test1234'
+    click_on 'Log in'
+
+    expect(page).to have_content 'Invalid Email or password.'
+  end
+end
