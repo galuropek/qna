37df8d855b7d8e063767c46dc5a54951999b6c48
galuropek <Z322200119i>
2021-12-02 15:33:33 +0300

    User can sign out

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
+  sequence :email do |n|
+    "user#{n}@mail.com"
+  end
+
   factory :user do
-    
+    email
+    password { 'test1234' }
+    password_confirmation { 'test1234' }
   end
 end
--- spec/features/sign_out_spec.rb
+++ spec/features/sign_out_spec.rb
@@ -1,1 +1,18 @@
 require 'rails_helper'
+
+feature 'User can sign out', %q{
+  In order to end the session
+  I`d like to be able to sign out
+} do
+
+  given(:user) { create(:user) }
+
+  scenario 'Authorized user logs out' do
+    sign_in(user)
+
+    visit root_path
+    click_on 'Logout'
+
+    expect(page).to have_content 'Signed out successfully.'
+  end
+end
