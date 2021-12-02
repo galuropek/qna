f5374bc248de6d2dda75c7f79b204350c495ad16
galuropek <Z322200119i>
2021-12-02 21:52:13 +0300

    User can sign up

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+feature 'User can sign up', %q{
+  Guest registers in the system 
+  to be able to log in and ask questions
+} do
+
+  given(:user) { create(:user) }
+
+  background { visit new_user_registration_path }
+
+  scenario 'New user registers in the system' do
+    fill_in 'Email', with: 'user@mail.com'
+    fill_in 'Password', with: 'test1234'
+    fill_in 'Password confirmation', with: 'test1234'
+    click_on 'Sign up'
+
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
+
+  scenario 'User already is registered in the system' do
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password confirmation', with: user.password_confirmation
+    click_on 'Sign up'
+
+    expect(page).to have_content 'Email has already been taken'
+  end
+
+  scenario 'User registers in the system with error' do
+    click_on 'Sign up'
+
+    expect(page).to have_content "Email can't be blank"
+  end
+end
