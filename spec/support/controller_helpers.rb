ae133f841fb5fe272cff9f2f44837d6f8cf80321
galuropek <Z322200119i>
2022-06-06 14:28:09 +0300

    attachments_controller tests

--- spec/support/controller_helpers.rb
+++ spec/support/controller_helpers.rb
@@ -3,4 +3,9 @@ module ControllerHelpers
     @request.env['devise.mapping'] = Devise.mappings[:user]
     sign_in(user)
   end
+
+  def attach_file_to(source)
+    source.files.attach(io: File.open("#{Rails.root}/spec/rails_helper.rb"),
+                        filename: 'rails_helper.rb', content_type: 'text/plain')
+  end
 end
